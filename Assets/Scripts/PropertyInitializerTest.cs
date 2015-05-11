d096cfe7c5a376b635057675442657cccfe9f987
alexzzzz <alexzzzz@live.com>
2015-05-12 02:46:57 +0300

    small changes to property initializers test

--- Assets/Scripts/PropertyInitializerTest.cs
+++ Assets/Scripts/PropertyInitializerTest.cs
@@ -3,11 +3,12 @@
 internal class PropertyInitializerTest : MonoBehaviour
 {
 	private string Property1 { get; } = "Hello, World! 111";
-	private string Property2 { get; } = "Hello, World! 222";
+	private string Property2 { get; set; } = "Hello, World! 222";
 
 	private void Start()
 	{
 		Debug.Log("Property1 value: " + Property1);
 		Debug.Log("Property2 value: " + Property2);
+		Property2 = "Bye, World!";
 	}
 }
