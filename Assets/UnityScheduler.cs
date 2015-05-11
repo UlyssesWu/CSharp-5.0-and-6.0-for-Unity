deea778c14b7c9b741db5c60254bbebc100af0fa
alexzzzz <alexzzzz@live.com>
2015-05-11 18:04:05 +0300

    initial commit

---
+++ Assets/UnityScheduler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class UnityScheduler : MonoBehaviour
+{
+	private UnitySynchronizationContext context;
+
+	private void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+
+		context = new UnitySynchronizationContext();
+		SynchronizationContext.SetSynchronizationContext(context);
+	}
+
+	private void LateUpdate()
+	{
+		context.Run();
+	}
+}
+
+public class UnitySynchronizationContext : SynchronizationContext
+{
+	private readonly BlockingCollection<KeyValuePair<SendOrPostCallback, object>> queue =
+		new BlockingCollection<KeyValuePair<SendOrPostCallback, object>>();
+
+	public override void Post(SendOrPostCallback d, object state)
+	{
+		queue.Add(new KeyValuePair<SendOrPostCallback, object>(d, state));
+	}
+
+	public void Run()
+	{
+		KeyValuePair<SendOrPostCallback, object> workItem;
+		while (queue.TryTake(out workItem))
+		{
+			workItem.Key(workItem.Value);
+		}
+	}
+}
---
+++ smcs/smcs/Program.cs
@@ -0,0 +1,162 @@
+﻿//#define LOG
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+internal class Program
+{
+	private enum CompilerVersion
+	{
+		Version3,
+		Version5,
+		Version6,
+	}
+
+	private static int Main(string[] args)
+	{
+		try
+		{
+			var compilationOptions = GetCompilationOptions(args);
+			var unityEditorDataDir = GetUnityEditorDataDir(compilationOptions);
+
+			InitLog();
+			Log($"Project directory: {Directory.GetCurrentDirectory()}");
+			Log($"Unity directory: {unityEditorDataDir}");
+
+			CompilerVersion compilerVersion;
+			if (Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "Roslyn")))
+			{
+				compilerVersion = CompilerVersion.Version6;
+			}
+			else if (compilationOptions.Any(line => line.Contains("AsyncBridge.Net35.dll")))
+			{
+				compilerVersion = CompilerVersion.Version5;
+			}
+			else
+			{
+				compilerVersion = CompilerVersion.Version3;
+			}
+			Log($"Compiler: {compilerVersion}");
+
+			var process = CreateCompilerProcess(compilerVersion, unityEditorDataDir, args[0]);
+			process.StartInfo.RedirectStandardError = true;
+			process.StartInfo.RedirectStandardOutput = true;
+			process.StartInfo.UseShellExecute = false;
+			process.Start();
+
+			Log($"Process: {process.StartInfo.FileName}");
+			Log($"Arguments: {process.StartInfo.Arguments}");
+
+			string output = process.StandardOutput.ReadToEnd();
+			string error = process.StandardError.ReadToEnd();
+			process.WaitForExit();
+
+			Console.Error.Write(error);
+			Console.Out.Write(output);
+
+			Log("\n- Compiler output: ------");
+			Log(output);
+			Log("\n- Compiler errors: ------");
+			Log(error);
+
+			if (process.ExitCode != 0 || compilerVersion != CompilerVersion.Version6)
+			{
+				return process.ExitCode;
+			}
+
+			Log("- Symbol DB convertion: -");
+
+			var pdb2mdbPath = Path.Combine(Directory.GetCurrentDirectory(), @"Roslyn/pdb2mdb.exe");
+			var libraryPath = Directory.GetFiles("Temp", "*.dll").First();
+			var pdbPath = Path.Combine("Temp", Path.GetFileNameWithoutExtension(Directory.GetFiles("Temp", "*.dll").First()) + ".pdb");
+
+			process = new Process();
+			process.StartInfo.FileName = pdb2mdbPath;
+			process.StartInfo.Arguments = libraryPath;
+			process.StartInfo.UseShellExecute = false;
+			process.StartInfo.RedirectStandardOutput = true;
+			process.StartInfo.CreateNoWindow = true;
+
+			Log($"Process: {process.StartInfo.FileName}");
+			Log($"Arguments: {process.StartInfo.Arguments}");
+
+			process.Start();
+			process.WaitForExit();
+			File.Delete(pdbPath);
+
+			Log("\n- pdb2mdb.exe output: ---");
+			Log(process.StandardOutput.ReadToEnd());
+			return 0;
+		}
+		catch (Exception e)
+		{
+			Console.Error.Write($"Compiler redirection error: {e.Message}\n{e.StackTrace}");
+			return -1;
+		}
+	}
+
+	private const string LOG_FILENAME = "log.txt";
+
+	[Conditional("LOG")]
+	private static void InitLog()
+	{
+		File.WriteAllText(LOG_FILENAME, "");
+	}
+
+	[Conditional("LOG")]
+	private static void Log(string message)
+	{
+		File.AppendAllText(LOG_FILENAME, message + "\n");
+	}
+
+	private static Process CreateCompilerProcess(CompilerVersion version, string unityEditorDataDir, string responseFile)
+	{
+		string processPath;
+		string processArguments;
+
+		var systemCoreDllPath = Path.Combine(unityEditorDataDir, @"Mono/lib/mono/2.0/System.Core.dll");
+
+		switch (version)
+		{
+			case CompilerVersion.Version3:
+				processPath = Path.Combine(unityEditorDataDir, @"Mono/bin/mono.exe");
+				var compilerPath = Path.Combine(unityEditorDataDir, @"Mono/lib/mono/2.0/gmcs.exe");
+				processArguments = $"\"{compilerPath}\" {responseFile}";
+				break;
+
+			case CompilerVersion.Version5:
+				processPath = Path.Combine(unityEditorDataDir, @"MonoBleedingEdge/lib/mono/4.5/mcs.exe");
+				processArguments = $"-sdk:2 -langversion:Future -r:\"{systemCoreDllPath}\" {responseFile}";
+				break;
+
+			case CompilerVersion.Version6:
+				processPath = Path.Combine(Directory.GetCurrentDirectory(), @"Roslyn/csc.exe");
+				var mscorlib = Path.Combine(unityEditorDataDir, @"Mono/lib/mono/2.0/mscorlib.dll");
+				processArguments = $"-nostdlib+ -noconfig -r:\"{mscorlib}\" -r:\"{systemCoreDllPath}\" {responseFile}";
+				break;
+
+			default:
+				throw new ArgumentOutOfRangeException(nameof(version), version, null);
+		}
+
+		var process = new Process();
+		process.StartInfo.FileName = processPath;
+		process.StartInfo.Arguments = processArguments;
+		return process;
+	}
+
+	private static string GetUnityEditorDataDir(string[] compilationOptions)
+	{
+		var filename = compilationOptions.First(line => line.Contains("UnityEngine.dll")).Substring(3).Trim('\"');
+		var dir = Directory.GetParent(filename).Parent;
+		return dir.FullName;
+	}
+
+	private static string[] GetCompilationOptions(string[] args)
+	{
+		var compilationOptions = File.ReadAllLines(args[0].TrimStart('@'));
+		return compilationOptions;
+	}
+}
