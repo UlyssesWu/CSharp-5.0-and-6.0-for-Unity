aa87efbcfe377df3f42663eb26e33fa14f5cea59
alexzzzz <alexzzzz@live.com>
2015-05-12 23:10:53 +0300

    added GUI to make it easier to see test results on mobile devices

---
+++ Assets/Scripts/Logger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+internal class Logger : MonoBehaviour
+{
+	private Text textControl;
+
+	private void Awake()
+	{
+		textControl = GetComponent<Text>();
+		Application.logMessageReceivedThreaded += Application_logMessageReceived;
+
+		Debug.Log("Current platform: " + Application.platform);
+	}
+
+	private void Application_logMessageReceived(string message, string stackTrace, LogType type)
+	{
+		textControl.text += message + "\n";
+	}
+}
