09b8c4e538ef375ece3bfe3f62aa29401e6e35e6
alexzzzz <alexzzzz@live.com>
2015-11-04 18:47:12 +0300

    commented out the lines that mcs.exe fails to compile

--- Assets/Demo/Scripts/NullConditionalTest.cs
+++ Assets/Demo/Scripts/NullConditionalTest.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-#warning Mono 4.0.3 compiler fails this test
 internal class NullConditionalTest : MonoBehaviour
 {
 	private void Start()
@@ -9,8 +8,12 @@ internal class NullConditionalTest : MonoBehaviour
 		var bar = foo.Length != 0 ? new[] { 1, 2, 3, 4, 5 } : null;
 
 		Debug.Log("Number of foos: " + foo?.Length);
+
+#warning Mono 4.0.3 compiler fails this test
+		/*
 		Debug.Log("3th bar has value: " + (bar?[3]).HasValue); // Mono 4.0.3 compiler fails here
 		   // error CS1061: Type `int' does not contain a definition for `HasValue' and no extension method `HasValue'
 		   // of type `int' could be found. Are you missing an assembly reference?
+		*/
 	}
 }
