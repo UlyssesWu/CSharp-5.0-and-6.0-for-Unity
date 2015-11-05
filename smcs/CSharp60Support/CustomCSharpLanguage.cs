8222d3a38899cc47c8b17852722324c38af154c2
alexzzzz <alexzzzz@live.com>
2015-11-05 20:29:07 +0300

    Unity 4 support for CSharp60Support project

--- smcs/CSharp60Support/CustomCSharpLanguage.cs
+++ smcs/CSharp60Support/CustomCSharpLanguage.cs
@@ -5,6 +5,21 @@ using UnityEditor.Scripting.Compilers;
 
 internal class CustomCSharpLanguage : CSharpLanguage
 {
+#if UNITY4
+	public override ScriptCompilerBase CreateCompiler(MonoIsland island, bool buildingForEditor, BuildTarget targetPlatform)
+	{
+		// This method almost exactly copies CSharpLanguage.CreateCompiler(...)
+
+		if (!buildingForEditor && targetPlatform.ToString().Contains("MetroPlayer") &&
+			(PlayerSettings.Metro.compilationOverrides == PlayerSettings.MetroCompilationOverrides.UseNetCore ||
+			 (PlayerSettings.Metro.compilationOverrides == PlayerSettings.MetroCompilationOverrides.UseNetCorePartially
+			  && !island._output.Contains("Assembly-CSharp-firstpass.dll"))))
+		{
+			return new MicrosoftCSharpCompiler(island);
+		}
+		return new CustomCSharpCompiler(island, false); // MonoCSharpCompiler is replaced with CustomCSharpCompiler
+	}
+#else
 	public override ScriptCompilerBase CreateCompiler(MonoIsland island, bool buildingForEditor, BuildTarget targetPlatform, bool runUpdater)
 	{
 		// This method almost exactly copies CSharpLanguage.CreateCompiler(...)
@@ -19,4 +34,5 @@ internal class CustomCSharpLanguage : CSharpLanguage
 		}
 		return new CustomCSharpCompiler(island, runUpdater); // MonoCSharpCompiler is replaced with CustomCSharpCompiler
 	}
+#endif
 }
