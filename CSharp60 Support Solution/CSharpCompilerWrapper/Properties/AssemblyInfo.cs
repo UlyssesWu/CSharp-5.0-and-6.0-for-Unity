6479ae42621066073a4c77e2addf069053338365
alexzzzz <alexzzzz@live.com>
2016-04-01 22:29:44 +0300

    pass -nologo option to roslyn instead of cutting out its logo from the output

--- CSharp60 Support Solution/CSharpCompilerWrapper/Properties/AssemblyInfo.cs
+++ CSharp60 Support Solution/CSharpCompilerWrapper/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.1")]
-[assembly: AssemblyFileVersion("1.6.1")]
+[assembly: AssemblyVersion("1.6.2")]
+[assembly: AssemblyFileVersion("1.6.2")]
