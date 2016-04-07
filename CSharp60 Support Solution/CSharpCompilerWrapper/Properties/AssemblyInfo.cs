5b81f6aa17d71ddbbe8eb315ae3791e38dfa2775
alexzzzz <alexzzzz@live.com>
2016-04-07 22:22:36 +0300

    Add experimental C# 7 support

--- CSharp60 Support Solution/CSharpCompilerWrapper/Properties/AssemblyInfo.cs
+++ CSharp60 Support Solution/CSharpCompilerWrapper/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6.2")]
-[assembly: AssemblyFileVersion("1.6.2")]
+[assembly: AssemblyVersion("1.6.3")]
+[assembly: AssemblyFileVersion("1.6.3")]
