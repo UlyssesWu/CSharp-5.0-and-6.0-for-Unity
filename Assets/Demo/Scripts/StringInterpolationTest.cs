c62db9951affd10710e5b37d2fabb29bf7684ae9
alexzzzz <alexzzzz@live.com>
2016-04-22 21:59:30 +0300

    Added FormattableString from CoreCLR to enable custom formatting of interpolated strings
    http://www.thomaslevesque.com/2015/02/24/customizing-string-interpolation-in-c-6/

--- Assets/Demo/Scripts/StringInterpolationTest.cs
+++ Assets/Demo/Scripts/StringInterpolationTest.cs
@@ -1,13 +1,54 @@
+using System;
 using UnityEngine;
 
 internal class StringInterpolationTest : MonoBehaviour
 {
-	private void Start()
-	{
-		var x = 100;
-		var y = 200;
+    private void Start()
+    {
+        var x = 100;
+        var y = 200;
 
-		Debug.Log($"x = {x}, y = {y}");
-		Debug.Log($"x > y = {x > y}");
-	}
+        Debug.Log($"x = {x}, y = {y}");
+        Debug.Log($"x > y = {x > y}");
+
+        var id = 100;
+        var name = "%Alice&";
+        string url = Url($"http://foobar/item/{id}/{name}");
+        Debug.Log(url);
+    }
+
+    private static string Url(FormattableString formattable)
+    {
+        return formattable.ToString(new UrlFormatProvider());
+    }
+}
+
+internal class UrlFormatProvider : IFormatProvider
+{
+    private readonly UrlFormatter formatter = new UrlFormatter();
+
+    public object GetFormat(Type formatType)
+    {
+        if (formatType == typeof(ICustomFormatter))
+        {
+            return formatter;
+        }
+        return null;
+    }
+
+    private class UrlFormatter : ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (arg == null)
+            {
+                return string.Empty;
+            }
+            if (format == "r")
+            {
+                return arg.ToString();
+            }
+            return Uri.EscapeDataString(arg.ToString());
+        }
+    }
 }
