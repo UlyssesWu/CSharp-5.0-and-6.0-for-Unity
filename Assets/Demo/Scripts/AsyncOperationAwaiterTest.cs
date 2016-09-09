6bda4c9c5458e37811ee3ad4908e3b938aa29912
alexzzzz <alexzzzz@live.com>
2016-09-09 17:04:06 +0300

    Repaired Unity 4 support

--- Assets/Demo/Scripts/AsyncOperationAwaiterTest.cs
+++ Assets/Demo/Scripts/AsyncOperationAwaiterTest.cs
@@ -1,42 +1,44 @@
-﻿using UnityEngine;
+﻿#if UNITY_5
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 
 public class AsyncOperationAwaiterTest : MonoBehaviour, IPointerClickHandler
 {
-    private Texture originalTexture;
-    private Texture2D texture;
-    private Material material;
+	private Texture originalTexture;
+	private Texture2D texture;
+	private Material material;
 
-    public async void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log("downloading...");
+	public async void OnPointerClick(PointerEventData eventData)
+	{
+		Debug.Log("downloading...");
 
-        var request = UnityWebRequest.Get("http://placeimg.com/512/512");
-        await request.Send();
-        Debug.Log("downloaded " + request.downloadedBytes + " bytes");
+		var request = UnityWebRequest.Get("http://placeimg.com/512/512");
+		await request.Send();
+		Debug.Log("downloaded " + request.downloadedBytes + " bytes");
 
-        texture.LoadImage(request.downloadHandler.data, true);
-        material.mainTexture = texture;
-    }
+		texture.LoadImage(request.downloadHandler.data, true);
+		material.mainTexture = texture;
+	}
 
-    private void Start()
-    {
-        material = GetComponent<Renderer>().sharedMaterial;
-        originalTexture = material.mainTexture;
-        texture = new Texture2D(512, 512);
+	private void Start()
+	{
+		material = GetComponent<Renderer>().sharedMaterial;
+		originalTexture = material.mainTexture;
+		texture = new Texture2D(512, 512);
 
-        Debug.Log("\n--> Click on the box to change its texture <--\n");
-    }
+		Debug.Log("\n--> Click on the box to change its texture <--\n");
+	}
 
-    private void OnDestroy()
-    {
-        Destroy(texture);
-        material.mainTexture = originalTexture;
-    }
+	private void OnDestroy()
+	{
+		Destroy(texture);
+		material.mainTexture = originalTexture;
+	}
 
-    private void Update()
-    {
-        transform.Rotate(0, 90 * Time.deltaTime, 0);
-    }
+	private void Update()
+	{
+		transform.Rotate(0, 90 * Time.deltaTime, 0);
+	}
 }
+#endif
